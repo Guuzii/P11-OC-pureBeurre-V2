9fb9254b5c5756e65a14d2fba2979fb90db72f4f
Erwan <morioe2809@gmail.com>
2020-10-09 14:26:23 +0200

    add model CommentAdmin

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Comment
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'message', 'product', 'date', 'is_validated')
+    list_filter = ('active', 'created_on')
+    search_fields = ('user.first_name', 'user.last_name', 'user.email', 'message')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(is_validated=True)
